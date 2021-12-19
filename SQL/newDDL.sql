dc87b87f5d03373b892389c194055c51c544b2a5
Shizhong-Shang <72275111+Shizhong-Shang@users.noreply.github.com>
2021-12-19 04:40:06 -0500

    Add files via upload

---
+++ SQL/newDDL.sql
@@ -0,0 +1,79 @@
+create table Book( 
+    ISBN              VARCHAR(25),
+    book              VARCHAR(50),
+    author            VARCHAR(50),
+    genre             VARCHAR(20),
+    publisher         VARCHAR(30),
+    import_price      NUMERIC(4,0),
+    in_stock          NUMERIC(4,0),
+    money_percentage  NUMERIC(4,2),
+    introduction      VARCHAR(50),
+    page_num          VARCHAR(10),
+    primary key (ISBN),
+    foreign key (publisher) references Publisher
+);
+
+CREATE TABLE Regested_User(
+    user_ID       NUMERIC(4,0),
+    user_name     VARCHAR(50),
+    user_password VARCHAR(20),
+    admin         BOOLEAN,     
+    primary key (user_ID)
+);
+
+CREATE TABLE Check_out(
+    track_num          NUMERIC(4,0),
+    user_ID            NUMERIC(4,0),
+    car_ID             NUMERIC(4,0),
+    curr_order_place   VARCHAR(30),
+    date_buy           NUMERIC(8,0),
+    shipping_company   VARCHAR(20),
+    shipping_address   VARCHAR(50),
+    billing_address    VARCHAR(50),
+    primary key (track_num),
+    foreign key (user_ID) references Regested_User,
+    foreign key (ISBN) references Book,
+    foreign key (car_ID) references Car
+);
+
+CREATE TABLE Car(
+    user_ISBN            VARCHAR(30),
+    car_ID               NUMERIC(4,0),
+    book_num             NUMERIC(4,0),
+    sell_price           NUMERIC(4,0),
+    primary key (user_ISBN)
+)
+
+CREATE TABLE Address(
+    address_ID    VARCHAR(10),
+    ZIP_code        VARCHAR(6),
+    user_ID         NUMERIC(4,0),
+    street_address  VARCHAR(50),
+    primary key (address_ID),
+    foreign key (user_ID) references Regested_User,
+    foreign key (ZIP_code) references Postcode
+);
+
+CREATE TABLE Postcode(
+    ZIP_code        VARCHAR(6),
+    province        VARCHAR(20),
+    city            VARCHAR(20),
+    primary key (ZIP_code),
+)
+
+CREATE TABLE Publisher(
+    publisher       VARCHAR(30),
+    address         VARCHAR(100),
+    email           VARCHAR(30),
+    phone_num       NUMERIC(14),
+    bank_account    NUMERIC(20),
+    primary key (publisher)
+);
+
+CREATE TABLE Record(
+    record_ID         VARCHAR(4,0),
+    genre_or_author   VARCHAR(20),
+    money             NUMERIC(4,0),
+    type              BOOLEAN,
+    primary key (record_ID, genre_or_author)
+);
---
+++ SQL/newinitialize.sql
@@ -0,0 +1,32 @@
+delete from Record;
+delete from Publisher;
+delete from Postcode;
+delete from Address;
+delete from Car;
+delete from Check_Out;
+delete from Regested_User;
+delete from Book;
+
+insert into Store_Book values ('1-86092-031-4','The FiveOrange Pipes','Arthur Conan Doyle','Crime','Good Publisher','14','80','5','asdfghjkl','556');
+insert into Store_Book values ('1-86092-038-4','The FiveOrange Pipes: Old Version','Katherine Mansfield','Classic','Nice Publisher','17','70','6','ertyuio','877');
+insert into Store_Book values ('1-86092-041-4','A Chance for Mr Enemy','Thomas Hardy','Crime','Another Publisher','15','100','7','jhgfd','334');
+insert into Store_Book values ('1-86092-021-7','A Chance for Mr Lever','Graham Greene','Classic','Good Publisher','20','30','8','uytrrew','567');
+insert into Store_Book values ('1-86092-045-4','A Mere Interlude','Thomas Hardy','Romance','New Publisher','22','40','9','mnbvcx','765');
+insert into Store_Book values ('1-86092-050-0','The Dancing Partner: Clocks','Jerome K. Jerome','Comedy','Old Publisher','17','60','10','zxcvb','234');
+
+insert into Regested_User values ('11111','Amy','12345','0');
+insert into Regested_User values ('22222','Bob','23456','0');
+insert into Regested_User values ('33333','Chris','34567','0');
+insert into Regested_User values ('44444','Dave','45678','0');
+insert into Regested_User values ('55555','Manager','56789','1');
+
+insert into Postcode values ('K1V3N6','ON','Ottawa');
+insert into Postcode values ('K1V1M2','ON','Toronto');
+insert into Postcode values ('K2E6K7','BC','Vancouver');
+insert into Postcode values ('K2E8L4','BC','Victoria');
+
+insert into Publisher values ('Good Publisher','11 GoodPublisher rd, Ottawa, ON, K1V1G1','GoodPublisher@gmail.com','1-613-263-1122','12341234');
+insert into Publisher values ('Nice Publisher','11 NicePublisher rd, Ottawa, ON, K1V2N2','NicePublisher@gmail.com','1-613-263-2233','34563456');
+insert into Publisher values ('Another Publisher','11 AnotherPublisher rd, Ottawa, ON, K1V3A3','AnotherPublisher@gmail.com','1-613-263-3344','56785678');
+insert into Publisher values ('New Publisher','11 NewPublisher rd, Ottawa, ON, K1V4N4','NewPublisher@gmail.com','1-613-263-4455','78907890');
+insert into Publisher values ('Old Publisher','11 OldPublisher rd, Ottawa, ON, K1V5O5','OldPublisher@gmail.com','1-613-263-5566','43214321');
